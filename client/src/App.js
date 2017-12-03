25c6a6f726c14f1b5b5c5da92dccd55ab940327a
Jasmontas Lukas <lukas.jasmontas@mif.stud.vu.lt>
2017-12-03 21:49:26 +0200

    Build fixes

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
   getBeerList() {
     this.setState({ isLoading: true });
 
-    return fetch('/beer/', {
+    return fetch(constants.API_URL + '/beer/', {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
