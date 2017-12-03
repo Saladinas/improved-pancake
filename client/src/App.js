8dab41b9a5d6c00bb691b1dca4601fdcf5b34b2a
Jasmontas Lukas <lukas.jasmontas@mif.stud.vu.lt>
2017-12-03 21:27:31 +0200

    Removed hardcoded values for development

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,14 @@ class App extends Component {
     super(props);
 
     this.state = {
-      // items: [],
-      // numberOfItems: 0,
-      numberOfItems: 4,
-      displayingItems: 4,
-      items: [{ "id": "1", "name": "Šviesusis", "bitterness": 3, "color": "light", "alc": 6 }, { "id": "2", "name": "Tamsusis", "bitterness": 5, "color": "dark", "alc": 8 }, { "id": "3", "name": "Karamelinis", "bitterness": 4, "color": "dark", "alc": 7 }, { "id": "4", "name": "Kvietinis", "bitterness": 3, "color": "light", "alc": 4 }],
-      itemsToDisplay: [{ "id": "1", "name": "Šviesusis", "bitterness": 3, "color": "light", "alc": 6 }, { "id": "2", "name": "Tamsusis", "bitterness": 5, "color": "dark", "alc": 8 }, { "id": "3", "name": "Karamelinis", "bitterness": 4, "color": "dark", "alc": 7 }, { "id": "4", "name": "Kvietinis", "bitterness": 3, "color": "light", "alc": 4 }],
+      items: [],
+      itemsToDisplay: [],
       selectedFilter: null,
     };
   }
 
   componentDidMount() {
-    // this.getBeerList();
+    this.getBeerList();
   }
 
   getBeerList() {
@@ -39,7 +35,7 @@ class App extends Component {
       .then((responseJson) => {
         this.setState({
           items: responseJson.items,
-          numberOfItems: responseJson.itemCount,
+          itemsToDisplay: responseJson.items,
         });
         return responseJson.items;
       })
